a3f4aca380713fb7ea3e66a020a9e17c70670817
Elena Sveshnikova <lenamarinkevich@mail.ru>
2022-09-08 14:58:06 +0300

    fear: interval

--- templates/js/index.js
+++ templates/js/index.js
@@ -1,15 +1,54 @@
-let select = document.querySelector('#request__input_4');
+// table
+const container = document.querySelector('.container')
 
+// buttons
+const findBtn = document.querySelector('.form__button_type_find')
+const resetBtn = document.querySelector('.form__button_type_reset')
 
-let data = `"Afghanistan"
-"South Africa (Republic of)"
-"Marion Island"
-"Angola (Republic of)"
-"Albania (Republic of)"`
-   
-let fileRows = data.toString().replace(/"/g , '').split('\n');
+// inputs chenges
+const intervalToApog = document.querySelector('.input-request_name_apogey')
+const intervalInputApog = document.querySelector('.interval_type_apogey')
 
-for (let i=0; i < fileRows.length; i++) {
- select.options[i+1] = new Option( `${fileRows[i]}`, `${fileRows[i]}`, false, false)
+const intervalToPerig =  document.querySelector('.input-request_name_perigey')
+const intervalInputPerig = document.querySelector('.interval_type_perigey')
+
+const intervalToInclin = document.querySelector('.input-request_name_inclin')
+const intervalInputInclin = document.querySelector('.interval_type_inclin')
+
+// open table with btn find
+function openTable() {
+    container.classList.add('container_opened');
+    console.log(1);
 }
-console.log(fileRows);
+
+findBtn.addEventListener('click', openTable);
+
+// change inputs intervals
+function intervalEqual(elem) {
+    elem.classList.add("form__interval_type_equal")
+}
+
+function intervalBlur(interval, inpTo) {
+    if (inpTo.value === "") {
+        interval.classList.remove("form__interval_type_equal")
+    }
+}
+
+intervalToApog.addEventListener('focus', () => intervalEqual(intervalInputApog))
+intervalToApog.addEventListener('blur', () => intervalBlur(intervalInputApog, intervalToApog))
+
+intervalToPerig.addEventListener('focus', () => intervalEqual(intervalInputPerig))
+intervalToPerig.addEventListener('blur', () => intervalBlur(intervalInputPerig, intervalToPerig))
+
+intervalToInclin.addEventListener('focus', () => intervalEqual(intervalInputInclin))
+intervalToInclin.addEventListener('blur', () => intervalBlur(intervalInputInclin, intervalToInclin))
+
+
+// click on reset
+function deleteEqual() {
+    intervalInputApog.classList.remove("form__interval_type_equal")
+    intervalInputPerig.classList.remove("form__interval_type_equal")
+    intervalInputInclin.classList.remove("form__interval_type_equal")
+}
+
+resetBtn.addEventListener('click', deleteEqual)
